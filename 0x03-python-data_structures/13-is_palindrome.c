902fd7b6499696af826ed9499b53ea3fda48846a
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-06-13 01:56:47 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,69 +1,112 @@
 #include "lists.h"
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
- *
- * Return: A pointer to the head of the reversed list.
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
-listint_t *reverse_listint(listint_t **head)
+size_t print_listint(const listint_t *h)
 {
-	listint_t *node = *head, *next, *prev = NULL;
+    const listint_t *current;
+    unsigned int n; /* number of nodes */
 
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
+    current = h;
+    n = 0;
+    while (current != NULL)
+    {
+        printf("%i\n", current->n);
+        current = current->next;
+        n++;
+    }
 
-	*head = prev;
-	return (*head);
+    return (n);
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+
+    return (new);
+}
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+    listint_t *current;
+
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
+/**
+ * is_palindrome- check the funchion of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 1if it is true
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
+    listint_t *current = *head;
+    int length = 0, i = 0, j;
+    int *values = NULL;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+    /* compute length and allocate array to store values */
+    while (current != NULL)
+    {
+        length++;
+        current = current->next;
+    }
+    values = (int *)malloc(length * sizeof(int));
+    if (values == NULL)
+        return (0);
 
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
+    /* fill array with values of nodes in the list */
+    current = *head;
+    for (i = 0; i < length; i++)
+    {
+        values[i] = current->n;
+        current = current->next;
+    }
 
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
+    /* check if the array is a palindrome */
+    for (i = 0, j = length - 1; i <= j; i++, j--)
+    {
+        if (values[i] != values[j])
+        {
+            free(values);
+            return (0);
+        }
+    }
 
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
-		return (0);
-
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
-	{
-		if (tmp->n != rev->n)
-			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
-	}
-	reverse_listint(&mid);
-
-	return (1);
+    free(values);
+    return (1);
 }
