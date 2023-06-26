abbf4b2e92b864f9246588cee6cd7d66a60ab751
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-06-26 14:05:41 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 def safe_print_list(my_list=[], x=0):
     
@@ -14,4 +13,5 @@ def safe_print_list(my_list=[], x=0):
         except:
             break
     print()
-    return count
+
+return count
