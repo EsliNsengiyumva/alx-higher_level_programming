1f6c1abfb0439d12d7d0136984bb7358e113b5d8
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-06-26 14:09:20 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,9 +8,12 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0,x):
         
         try:
+
             print(my_list[i], end="")
             count += 1
+
         except:
+
             break
     print()
 
