15439f8e718ef92329a1b1d6077597ff1e1175e4
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-06-27 14:23:48 -0700

    commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
+"""Check type"""
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """Private instance attribute: size
+    Instantiation with optional """
+
     def __init__(self, size=0):
-        """
-        the initialization function for the square class
-        checks for input errors for size
-        """
-        if type(size) != int:
+        """Initializes attribute size """
+
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
