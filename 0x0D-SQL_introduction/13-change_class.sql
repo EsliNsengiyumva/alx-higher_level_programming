5550613ae0f6dca2bd18b406e4c84ac9585f063f
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-08-16 13:18:59 -0700

    updateed

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,5 @@
 i--removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
-DELETE FROM `second_table`
-WHERE `score` <= 5;
+USE hbtn_0c_0;
+
+DELETE FROM second_table
+WHERE score <= 5;
