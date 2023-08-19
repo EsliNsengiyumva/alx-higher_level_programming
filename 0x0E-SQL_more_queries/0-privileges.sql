dd50499746a093dd2c3818fe6b113ccf80a7b3ff
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-08-19 16:11:44 -0700

    updated

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,9 @@
 -- Connect to MySQL server on localhost
 mysql -u root -p
-
 -- Run the following queries to check privileges for 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
 -- Run the following queries to check privileges for 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
 -- Exit MySQL prompt
 exit
 
